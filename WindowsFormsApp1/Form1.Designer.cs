926fdb2fb9a4f55350908d44d9f11d6c22f61aa6
abduraghmaan <abduraghmaangabriels2@gmail.com>
2020-02-16 23:59:46 +0200

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.lblEpisodeNumber = new System.Windows.Forms.Label();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.txfFilesRename = new System.Windows.Forms.TextBox();
+            this.lblNameFormat = new System.Windows.Forms.Label();
+            this.tblFiles = new System.Windows.Forms.ListView();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.nudEpisodeNumber = new System.Windows.Forms.NumericUpDown();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.chkEpisodeSuffix = new System.Windows.Forms.CheckBox();
+            this.btnAddFile = new System.Windows.Forms.Button();
+            this.tblOrdered = new System.Windows.Forms.ListView();
+            this.btnShiftLeft = new System.Windows.Forms.Button();
+            this.btnShiftRight = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudEpisodeNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(664, 365);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 0;
+            this.btnEdit.Text = "btnEdit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEditClick);
+            // 
+            // lblEpisodeNumber
+            // 
+            this.lblEpisodeNumber.AutoSize = true;
+            this.lblEpisodeNumber.Location = new System.Drawing.Point(9, 420);
+            this.lblEpisodeNumber.Name = "lblEpisodeNumber";
+            this.lblEpisodeNumber.Size = new System.Drawing.Size(88, 13);
+            this.lblEpisodeNumber.TabIndex = 1;
+            this.lblEpisodeNumber.Text = "episode number :";
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Location = new System.Drawing.Point(664, 27);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFolder.TabIndex = 2;
+            this.btnOpenFolder.Text = "Open Folder";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.btnOpenFolderClick);
+            // 
+            // txfFilesRename
+            // 
+            this.txfFilesRename.Location = new System.Drawing.Point(664, 339);
+            this.txfFilesRename.Name = "txfFilesRename";
+            this.txfFilesRename.Size = new System.Drawing.Size(100, 20);
+            this.txfFilesRename.TabIndex = 5;
+            // 
+            // lblNameFormat
+            // 
+            this.lblNameFormat.AutoSize = true;
+            this.lblNameFormat.Location = new System.Drawing.Point(664, 226);
+            this.lblNameFormat.Name = "lblNameFormat";
+            this.lblNameFormat.Size = new System.Drawing.Size(108, 13);
+            this.lblNameFormat.TabIndex = 6;
+            this.lblNameFormat.Text = "Series Name Format :";
+            // 
+            // tblFiles
+            // 
+            this.tblFiles.FullRowSelect = true;
+            this.tblFiles.Location = new System.Drawing.Point(12, 12);
+            this.tblFiles.Name = "tblFiles";
+            this.tblFiles.Size = new System.Drawing.Size(324, 376);
+            this.tblFiles.TabIndex = 8;
+            this.tblFiles.UseCompatibleStateImageBehavior = false;
+            this.tblFiles.View = System.Windows.Forms.View.Details;
+            this.tblFiles.SelectedIndexChanged += new System.EventHandler(this.tblFilesSelectedIndexChanged);
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(13, 476);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(28, 13);
+            this.lblPath.TabIndex = 9;
+            this.lblPath.Text = "path";
+            // 
+            // nudEpisodeNumber
+            // 
+            this.nudEpisodeNumber.Location = new System.Drawing.Point(126, 450);
+            this.nudEpisodeNumber.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudEpisodeNumber.Name = "nudEpisodeNumber";
+            this.nudEpisodeNumber.Size = new System.Drawing.Size(120, 20);
+            this.nudEpisodeNumber.TabIndex = 10;
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Location = new System.Drawing.Point(583, 543);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(75, 23);
+            this.btnPreview.TabIndex = 11;
+            this.btnPreview.Text = "preview";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreviewClick);
+            // 
+            // chkEpisodeSuffix
+            // 
+            this.chkEpisodeSuffix.AutoSize = true;
+            this.chkEpisodeSuffix.Location = new System.Drawing.Point(664, 302);
+            this.chkEpisodeSuffix.Name = "chkEpisodeSuffix";
+            this.chkEpisodeSuffix.Size = new System.Drawing.Size(91, 17);
+            this.chkEpisodeSuffix.TabIndex = 12;
+            this.chkEpisodeSuffix.Text = "Episode Suffx";
+            this.chkEpisodeSuffix.UseVisualStyleBackColor = true;
+            this.chkEpisodeSuffix.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // btnAddFile
+            // 
+            this.btnAddFile.Location = new System.Drawing.Point(664, 70);
+            this.btnAddFile.Name = "btnAddFile";
+            this.btnAddFile.Size = new System.Drawing.Size(75, 23);
+            this.btnAddFile.TabIndex = 13;
+            this.btnAddFile.Text = "button1";
+            this.btnAddFile.UseVisualStyleBackColor = true;
+            this.btnAddFile.Click += new System.EventHandler(this.btnAddFileClick1);
+            // 
+            // tblOrdered
+            // 
+            this.tblOrdered.FullRowSelect = true;
+            this.tblOrdered.Location = new System.Drawing.Point(402, 12);
+            this.tblOrdered.Name = "tblOrdered";
+            this.tblOrdered.Size = new System.Drawing.Size(245, 376);
+            this.tblOrdered.TabIndex = 14;
+            this.tblOrdered.UseCompatibleStateImageBehavior = false;
+            this.tblOrdered.View = System.Windows.Forms.View.Details;
+            this.tblOrdered.SelectedIndexChanged += new System.EventHandler(this.TblOrdered_SelectedIndexChanged);
+            // 
+            // btnShiftLeft
+            // 
+            this.btnShiftLeft.Location = new System.Drawing.Point(342, 154);
+            this.btnShiftLeft.Name = "btnShiftLeft";
+            this.btnShiftLeft.Size = new System.Drawing.Size(53, 23);
+            this.btnShiftLeft.TabIndex = 15;
+            this.btnShiftLeft.Text = "<";
+            this.btnShiftLeft.UseVisualStyleBackColor = true;
+            this.btnShiftLeft.Click += new System.EventHandler(this.BtnShiftLeft_Click);
+            // 
+            // btnShiftRight
+            // 
+            this.btnShiftRight.Location = new System.Drawing.Point(343, 184);
+            this.btnShiftRight.Name = "btnShiftRight";
+            this.btnShiftRight.Size = new System.Drawing.Size(53, 23);
+            this.btnShiftRight.TabIndex = 16;
+            this.btnShiftRight.Text = ">";
+            this.btnShiftRight.UseVisualStyleBackColor = true;
+            this.btnShiftRight.Click += new System.EventHandler(this.BtnShiftRight_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 719);
+            this.Controls.Add(this.btnShiftRight);
+            this.Controls.Add(this.btnShiftLeft);
+            this.Controls.Add(this.tblOrdered);
+            this.Controls.Add(this.btnAddFile);
+            this.Controls.Add(this.chkEpisodeSuffix);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(this.nudEpisodeNumber);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.tblFiles);
+            this.Controls.Add(this.lblNameFormat);
+            this.Controls.Add(this.txfFilesRename);
+            this.Controls.Add(this.btnOpenFolder);
+            this.Controls.Add(this.lblEpisodeNumber);
+            this.Controls.Add(this.btnEdit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudEpisodeNumber)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Label lblEpisodeNumber;
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.TextBox txfFilesRename;
+        private System.Windows.Forms.Label lblNameFormat;
+        private System.Windows.Forms.ListView tblFiles;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.NumericUpDown nudEpisodeNumber;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.CheckBox chkEpisodeSuffix;
+        private System.Windows.Forms.Button btnAddFile;
+        private System.Windows.Forms.ListView tblOrdered;
+        private System.Windows.Forms.Button btnShiftLeft;
+        private System.Windows.Forms.Button btnShiftRight;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        List<String> activityLog = new List<String>();
+        List<VideoFile> vfiles = new List<VideoFile>();
+        List<VideoFile> orderedfiles = new List<VideoFile>();
+
+        VideoFile selectedFile;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public String activityString(String componentName,String componentAction) {
+            String activity = componentName + " : " + componentAction;
+            activityLog.Add(activity);
+            return activity;
+        }
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void preview() {
+            foreach (VideoFile file in orderedfiles) {
+                //Console.WriteLine(txfFilesRename.Text);
+                if (!txfFilesRename.Text.Equals(""))
+                {
+                   file.setTempName(txfFilesRename.Text);
+                }
+                file.setEpisodeSuffix(chkEpisodeSuffix.Checked);
+                if (file.isEdited())
+                {
+                    file.setNumber(file.getTempNumber());
+                }
+                Console.WriteLine(file.formatName());
+                    //file.setName(file.getTempName());
+
+            }
+            int counter = 0;
+            foreach (ListViewItem item in tblOrdered.Items) {
+                //item.SubItems[0].Text = vfiles[counter].getNumber()+"";
+                //item.SubItems[1].Text = vfiles[counter].getName();
+                item.SubItems[0].Text = orderedfiles[counter].getTempNumber() + "";
+                //Console.WriteLine(vfiles[counter].formatName());
+                item.SubItems[1].Text = orderedfiles[counter].formatName();
+
+                counter++;
+            }
+        }
+        private void btnEditClick(object sender, EventArgs e)
+        {
+            //lblEpisodeNumber.Text = activityString("Button1","Clicked");
+            if (tblOrdered.FocusedItem != null)
+            {
+                int sel = tblOrdered.FocusedItem.Index;
+                selectedFile = orderedfiles[sel];
+                selectedFile.setEdited();
+                int tempNum = (int)nudEpisodeNumber.Value;
+                selectedFile.setTempNumber(tempNum);
+            }
+            recompileList();
+        }
+
+        public class VideoFile {
+            String name;
+            String tempname;
+            String extension;
+            String folder;
+            String tempfolder;
+            String path;
+            String temppath;
+            bool edited;
+            int number;
+            int tempnumber;
+
+
+            bool episodesuffix;
+            bool seasonSuffix;
+            public VideoFile(String file,int number) {
+                //this.path = file;
+                String[] str = file.Split('\\');
+                String[] fileName = (str[str.Length-1]).Split('.');
+
+                this.setName(fileName[0]);
+                this.tempname = this.name;
+                this.number = number;
+                this.tempnumber = number;
+                this.setExt(fileName[1]);
+
+                this.setPath(file);
+                this.temppath = path;
+
+                this.setFolder(str[str.Length - 2]);
+                this.tempfolder = this.folder;
+            }
+            //Mutator Methods
+            public void setName(String name) {this.name = name;}
+            public void setExt(String extension) {this.extension = extension;}
+            public void setNumber(int number) {this.number = number;}
+            public void setFolder(String folder) {this.folder = folder;}
+            public void setPath (String path) {this.path = path; }
+            public void setEdited() {this.edited = true;}
+
+            public void setTempNumber(int number) {this.tempnumber = number;}
+            public void setTempName(String name) { this.tempname = name; }
+            public void setTempFolder(String folder) { this.tempfolder = folder; }
+            public void setTempPath(String path) {this.temppath = path;}
+
+            public void setEpisodeSuffix(bool input) { this.episodesuffix = input; }
+            public void setSeasonSuffix(bool input) { this.seasonSuffix = input; }
+            public String formatName() {
+                String finalstr = "";
+                string suffix = "";
+                if (this.episodesuffix)
+                    suffix += " - Episode";
+                finalstr = this.tempname + suffix + " " +this.tempnumber;
+                return (finalstr);
+            }
+
+            //Accessor Methods
+            
+            //String name
+            public String getName() {return this.name;}
+            //String extension;
+            public String getExt() {return this.extension;}
+            //int number;
+            public int getNumber() {return this.number;}
+            //String folder;
+            public String getFolder() { return this.folder; }
+            //String path;
+            public String getPath() { return this.path; }
+            //bool edited;
+            public bool isEdited() {return this.edited;}
+
+            //int tempnumber;
+            public int getTempNumber(){return this.tempnumber;}
+            //String tempname;
+            public String getTempName() { return this.tempname; }
+            //String tempfolder;
+            public String getTempFolder() { return this.tempfolder; }
+            //String temppath;
+            public String getTempPath() { return this.temppath; }
+            //bool episodesuffix;
+            public bool hasEpisodeSuffix() { return this.episodesuffix; }
+            //bool seasonSuffix;
+            public bool hasSeasonSuffix(){ return this.seasonSuffix;}
+
+        }
+        private void btnOpenFolderClick(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    String[] files = Directory.GetFiles(fbd.SelectedPath);
+                    tblFiles.Columns.Add("Episode", 50);
+                    tblFiles.Columns.Add("Name", 400);
+
+                    tblOrdered.Columns.Add("Temp Episode", 50);
+                    tblOrdered.Columns.Add("Temp Name", 400);
+                    int count = 1;
+                    foreach(String s in files) {
+                        vfiles.Add(new VideoFile(s,count));
+                        count++;
+                    }
+                    foreach (VideoFile file in vfiles)
+                    {
+                        addListItem(false,createNewListViewItem(false,file));
+                        count++;
+                    }
+                    //System.Windows.Forms.MessageBox.Show(allFileNames,"Message");
+                    //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                }
+            }
+        }
+        public void addListItem(bool ordered,ListViewItem item) {
+            if(ordered)
+                tblOrdered.Items.Add(item);
+            else
+                tblFiles.Items.Add(item);
+        }
+        public ListViewItem createNewListViewItem(bool ordered,VideoFile file) {
+            string episode;
+            string name;
+            if (!ordered)
+            {
+                episode = file.getNumber() + "";
+                name = file.getName();
+            }
+            else
+            {
+                episode = file.getTempNumber() + "";
+                name = file.formatName();
+            }
+            return new ListViewItem(new[] { episode, name});
+        }
+        public void swapListItems(int a,int  b)
+        {
+            //VideoFile temp = new VideoFile("dud\dud.dud", vfiles.Count+1);
+            VideoFile temp = vfiles[a];
+            vfiles[a] = vfiles[b];
+            vfiles[b] = temp;
+        }
+        public void recompileList() {
+            //tblFiles.DataSource = null;
+            tblFiles.Items.Clear();
+            tblOrdered.Items.Clear();
+            //tblFiles.Clear();
+            foreach (VideoFile file in vfiles)
+            {
+                addListItem(false,createNewListViewItem(false,file));
+            }
+            foreach (VideoFile file in orderedfiles)
+            {
+                addListItem(true, createNewListViewItem(true,file));
+            }
+            //vfiles.Sort();
+        }
+
+        private void tblFilesSelectedIndexChanged(object sender, EventArgs e)
+        {
+            int sel = tblFiles.FocusedItem.Index;
+            selectedFile = vfiles[sel];
+            lblEpisodeNumber.Text = selectedFile.getName();
+            nudEpisodeNumber.Value = selectedFile.getNumber();
+            if (!txfFilesRename.Text.Equals(""))
+            {
+                selectedFile.setTempName(txfFilesRename.Text);
+            }
+            
+        }
+
+        private void btnPreviewClick(object sender, EventArgs e)
+        {
+            preview();
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnAddFileClick1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnShiftLeft_Click(object sender, EventArgs e)
+        {
+            if (tblOrdered.FocusedItem != null)
+            {
+                int sel = tblOrdered.FocusedItem.Index;
+                selectedFile = orderedfiles[sel];
+                selectedFile.setNumber(vfiles.Count + 1);
+                vfiles.Add(selectedFile);
+                orderedfiles.Remove(selectedFile);
+                //tblFiles.FocusedItem.Remove();
+                recompileList();
+
+                //tblOrdered.Focus = tblOrdered.Items.Count;
+            }
+        }
+
+        private void BtnShiftRight_Click(object sender, EventArgs e)
+        {
+            if (tblFiles.FocusedItem != null) {
+                int sel = tblFiles.FocusedItem.Index;
+                selectedFile = vfiles[sel];
+                selectedFile.setTempNumber(orderedfiles.Count + 1);
+                orderedfiles.Add(selectedFile);
+                vfiles.Remove(selectedFile);
+                //tblFiles.FocusedItem.Remove();
+                recompileList();
+                
+                //tblOrdered.Focus = tblOrdered.Items.Count;
+            }
+        }
+
+        private void TblOrdered_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int sel = tblOrdered.FocusedItem.Index;
+            selectedFile = orderedfiles[sel];
+            lblEpisodeNumber.Text = selectedFile.getTempName();
+            nudEpisodeNumber.Value = selectedFile.getTempNumber();
+            if (!txfFilesRename.Text.Equals(""))
+            {
+                selectedFile.setTempName(txfFilesRename.Text);
+            }
+        }
+    }
+}
